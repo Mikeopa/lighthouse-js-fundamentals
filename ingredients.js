b90b48f7e9e69d7b3234f446cb5e7b46c5c7f035
Michael Opadotun <mopadotun.cphomes@gmail.com>
2023-04-29 08:45:16 -0600

    The for and while loop

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// While loop to print out the contents of ingredients
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// For loop to print out the contents of ingredients
+for (let j = 0; j < ingredients.length; j++) {
+  console.log(ingredients[j]);
+}
+
+// Loop to print out the contents of ingredients backwards
+for (let k = ingredients.length - 1; k >= 0; k--) {
+  console.log(ingredients[k]);
+}
+
